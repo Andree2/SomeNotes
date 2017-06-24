3d4a468326554fe155dd84f98e8957caa1643712
gutsle <gutsle@users.noreply.github.com>
2017-06-24 14:38:08 +0100

    oh here is more php7 stuff but in a sensitive file

--- php/db.php
+++ php/db.php
@@ -1,9 +1,17 @@
 <?php
-$DBServer="localhost";
-$DBUsername="root";
-$DBPassword="";
-$DBDatabase="lifelog";
-$DBLink= mysql_connect($DBServer, $DBUsername, $DBPassword);
-mysql_set_charset('utf8', $DBLink);
-@mysql_select_db($DBDatabase) or die( "Unable to select database");
+	$DBServer="localhost";
+	$DBUsername="admin";
+	$DBPassword="";
+	$DBDatabase="lifelog";
+	$DBLink= mysqli_connect($DBServer, $DBUsername, $DBPassword);
+
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	if (!mysqli_set_charset($DBLink, 'utf8')) {
+		printf("Error setting charset to utf8: %s\n", mysqli_error($DBLink));
+		exit();
+	}
+
+	@mysqli_select_db($DBLink, $DBDatabase) or die( "Unable to select database");
 ?>
